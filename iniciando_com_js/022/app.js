2f55e9ab2d2714d8a5a0298c5623bc2ac7b121dc
Djavan Camargo <djavanoc@gmail.com>
2022-02-22 19:44:30 -0300

    Executar uma Função com Eventos
    como fazer um botão com JS.

---
+++ iniciando_com_js/022/app.js
@@ -0,0 +1,10 @@
+let nome = document.getElementById('nome').value;
+
+document.getElementById('nomeInput').innerHTML = nome;
+//função para quando escrever um nome e apertar o botão,trocara o nome
+function pegaNome(){
+  let nome = document.getElementById('nome').value;
+
+  document.getElementById('nomeInput').innerHTML = nome;
+
+}
